84ea1c20a9650652fbd5e6bb448b2790fb5833e4
AEscY <144313393+AEscY@users.noreply.github.com>
2026-09-14 00:50:22 +0800

    更新 states.py

--- ui/states.py
+++ ui/states.py
@@ -10,10 +10,8 @@ class CoinSG(StatesGroup):
     panel = State()
     grid = State()
     dip = State()
-    # 网格参数输入状态
     grid_edit_range = State()
     grid_edit_num = State()
-    # 低吸高卖参数输入状态
     dip_edit_buy = State()
     dip_edit_sell = State()
     dip_edit_base = State()
