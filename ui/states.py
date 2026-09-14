b041a3660d662229500a6e5b2d9de6ea324feb65
AEscY <144313393+AEscY@users.noreply.github.com>
2026-09-14 10:52:27 +0800

    更新 states.py

--- ui/states.py
+++ ui/states.py
@@ -1,17 +1,21 @@
 from aiogram.filters.state import StatesGroup, State
 
 
-class MainSG(StatesGroup):
+class AppSG(StatesGroup):
+    # 主菜单
     menu = State()
     add_coin = State()
 
+    # 币种面板
+    coin_panel = State()
 
-class CoinSG(StatesGroup):
-    panel = State()
+    # 网格
     grid = State()
-    dip = State()
     grid_edit_range = State()
     grid_edit_num = State()
+
+    # 低吸高卖
+    dip = State()
     dip_edit_buy = State()
     dip_edit_sell = State()
     dip_edit_base = State()
