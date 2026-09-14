b2bea6dabd4070269d83db75491404ea2ce98f76
AEscY <144313393+AEscY@users.noreply.github.com>
2026-09-14 15:21:21 +0800

    更新 states.py

--- ui/states.py
+++ ui/states.py
@@ -7,3 +7,7 @@ class AppSG(StatesGroup):
     coin_panel = State()
     grid = State()
     dip = State()
+    dip_edit_tp = State()
+    dip_edit_sl = State()
+    dip_edit_trailing = State()
+    dip_edit_spend = State()
