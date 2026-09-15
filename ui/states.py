7cce5c7571a3b2544c3db5d4a7feb0eba7f77564
AEscY <144313393+AEscY@users.noreply.github.com>
2026-09-16 01:28:16 +0800

    更新 states.py

--- ui/states.py
+++ ui/states.py
@@ -5,7 +5,6 @@ class AppSG(StatesGroup):
     menu = State()
     add_coin = State()
     coin_panel = State()
-    grid = State()
     dip = State()
-    params = State()        # 参数列表
-    edit_param = State()    # 编辑单个参数
+    params = State()
+    edit_param = State()
