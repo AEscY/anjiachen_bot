b57d0cfd24b67e00768bfdd6e759e27f337a6b71
AEscY <144313393+AEscY@users.noreply.github.com>
2026-09-16 01:44:25 +0800

    更新 states.py

--- ui/states.py
+++ ui/states.py
@@ -5,6 +5,7 @@ class AppSG(StatesGroup):
     menu = State()
     add_coin = State()
     coin_panel = State()
-    dip = State()
+    signal_detail = State()
+    position_detail = State()
     params = State()
     edit_param = State()
